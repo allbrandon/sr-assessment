9cd66925d19a4ef1c3310d7960ad43591debae73
Brandon Nguyen <z5205609@student.unsw.edu.au>
2021-03-23 11:57:05 +1100

    updated file location for heroku

--- api/uploadedfiles.py
+++ api/uploadedfiles.py
@@ -1,9 +1,11 @@
 from flask import send_from_directory
 from flask import current_app as app
 from flask_restx import Resource
+import os 
 
+MYDIR = os.path.dirname(__file__)
 
 class UploadedFilesApi(Resource):
     def get(self, filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'],
+        return send_from_directory(MYDIR + "/" + app.config['UPLOAD_FOLDER'],
                                filename)
